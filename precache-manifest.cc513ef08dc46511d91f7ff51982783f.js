0c35690a0f5a5a6d27369288cb22f0bc4004f71e
ZZC19951024 <zhu.zic@husky.neu.edu>
2020-12-02 11:26:59 +0800

    Updates

---
+++ precache-manifest.cc513ef08dc46511d91f7ff51982783f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d2b877f0350d33036e1c634ae91c40",
+    "url": "/seainfo6150-final-project-webapp/index.html"
+  },
+  {
+    "revision": "f0a9cb5ff31983713df0",
+    "url": "/seainfo6150-final-project-webapp/static/css/main.af39e4eb.chunk.css"
+  },
+  {
+    "revision": "b31f1711357fa972850e",
+    "url": "/seainfo6150-final-project-webapp/static/js/2.69bfc2ec.chunk.js"
+  },
+  {
+    "revision": "f0a9cb5ff31983713df0",
+    "url": "/seainfo6150-final-project-webapp/static/js/main.f006d963.chunk.js"
+  },
+  {
+    "revision": "940ec4a42937c37ae625",
+    "url": "/seainfo6150-final-project-webapp/static/js/runtime~main.c4d7f5e8.js"
+  },
+  {
+    "revision": "82c3403ddfd167db2dea4b343f85f223",
+    "url": "/seainfo6150-final-project-webapp/static/media/logo.82c3403d.png"
+  }
+]);
